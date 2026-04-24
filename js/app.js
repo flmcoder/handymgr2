7085425d6081bf0a9abd8cf6c744e9c619f2e5b5
flmcoder <aaron@flraz.com>
2026-04-24 10:04:43 -0700

    Bump app version from v9.6.8 to v9.6.9

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ var BRAND_LOGO_DEFAULT = 'assets/logo.png';
 var BRAND_LOGO_FALLBACK = 'https://pfst.cf2.poecdn.net/base/image/6ac452e679a06edc3e17d0dae13fac303de2fdbb970c22eb302651f44c558416?w=1996&h=938';
 var PORTAL_BRAND_NAME_DEFAULT = 'Fort Lowell Realty Tech Dispatch';
 var PORTAL_BRAND_LOGO_DEFAULT = 'https://pfst.cf2.poecdn.net/base/image/57c851c04753092259d83d0a1aa34e2fd889c7218b50a338e6100dbf21ae922c?w=733&h=982';
-var APP_VERSION = 'v9.6.8';
+var APP_VERSION = 'v9.6.9';
 var SERVER_VERSION = '';
 var VERSION_MISMATCH_TIMER = null;
 
